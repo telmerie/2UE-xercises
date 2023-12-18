df954f089dc62507176dfc89c21be3bfc6f3b251
Tes <tes_7583@hotmail.com>
2023-12-18 14:43:23 +0100

    restructure code

---
+++ Examples/Datatypes.cs
@@ -0,0 +1,14 @@
+class Datatypes
+{
+
+    static void Main(string[] args)
+    {
+
+        string x = Console.ReadLine(), y = Console.ReadLine(), z = Console.ReadLine();
+
+        Console.WriteLine($"x i {x}, y is {y}, z is {z}");
+
+    }
+
+
+}
