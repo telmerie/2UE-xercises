ec6e6817cb57764be556a26acb62385f0e4a1f8e
Tes <tes_7583@hotmail.com>
2024-01-12 13:56:45 +0100

    opgaver eksempler

--- Examples/Arrays.cs
+++ Examples/Arrays.cs
@@ -3,15 +3,68 @@ class Arrays
 
     static void Main()
     {
-        int[] numbers = new int[10];
+        //Opgave 5
+        // Console.Write("Input the number of elements to be stored in the array: ");
+        // int arraySize = Convert.ToInt32(Console.ReadLine());
 
+        // int[] intArray = new int[arraySize];
 
-        for(int i = 0; i < numbers.Length; i++){
-            numbers[i] = i;
+        // Console.WriteLine($"Input {arraySize} elements in the array :");
+        // for(int i = 0; i < intArray.Length; i++){
+        //     Console.Write($"element - {i} : ");
+        //     intArray[i] = Convert.ToInt32(Console.ReadLine());
+        // }
+
+        // int dublicates = 0;
+
+        // for(int i = 0; i < intArray.Length - 1; i++){
+        //     for(int j = i + 1; j < intArray.Length; j++){
+        //         if(intArray[i] == intArray[j]){
+        //             dublicates++;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // Console.WriteLine("Total number of duplicate elements found in the array is : " + dublicates);
+
+        //___________________________________________'
+        //Opgave 6
+
+        Console.Write("Input the number of elements to be stored in the array: ");
+        int arraySize = Convert.ToInt32(Console.ReadLine());
+
+        int[] intArray = new int[arraySize];
+
+        Console.WriteLine($"Input {arraySize} elements in the array :");
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            Console.Write($"element - {i} : ");
+            intArray[i] = Convert.ToInt32(Console.ReadLine());
         }
 
-        for(int i = 0; i < numbers.Length; i++){
-            Console.WriteLine(numbers[i]);
+        Console.WriteLine("The unique elements found in the array are :");
+
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            for (int j = 0; j < intArray.Length; j++)
+            {
+
+                if (intArray[i] == intArray[j] && i != j)
+                {
+                    break;
+                }
+
+                if (j == intArray.Length - 1)
+                {
+                    Console.Write(intArray[i] + " ");
+                }
+
+            }
+
         }
+
+
+
     }
 }
